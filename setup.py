a36f093191fb37784c44d535a8259627b5211737
Fabian Baier <fabian.baier1987@gmail.com>
2018-09-10 11:02:20 -0700

    test new setup.py

--- setup.py
+++ setup.py
@@ -1,130 +1,173 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
 """
-Setup module for the dcos_jupyter_tensorboard_extension
-"""
-from __future__ import print_function
-import setuptools
-from setuptools import setup, Command
-from setuptools.command.sdist import sdist
-from setuptools.command.build_py import build_py
-from setuptools.command.egg_info import egg_info
-from subprocess import check_call
-import glob
-import os
-import sys
-from os.path import join as pjoin
-from setupbase import (
-    create_cmdclass, ensure_python, find_packages
-    )
-from distutils.sysconfig import get_python_lib; 
 
-here = os.path.dirname(os.path.abspath(__file__))
-node_root = pjoin(here, 'labextension', 'dcos-jupter-tensorboard-labextension')
-python_lib = get_python_lib()
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
 
-#data_files_spec = [
-#    ('etc/jupyter/jupyter_notebook_config.d',
-#     'serverextension', 'dcos_jupyter_tensorboard_serverextension.json'),
-#     (python_lib, 'serverextension', 'dcos_jupyter_tensorboard_serverextension'),
-#    ('share/jupyter/lab/extensions',
-#     'labextension', 'dcos_jupyter_tensorboard_serverextension.json'),
-#]
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
-#cmdclass = create_cmdclass(data_files_spec=data_files_spec)
+setup(
+    # This is the name of your project. The first time you publish this
+    # package, this name will be registered for you. It will determine how
+    # users can install this project, e.g.:
+    #
+    # $ pip install sampleproject
+    #
+    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
+    #
+    # There are some restrictions on what makes a valid project name
+    # specification here:
+    # https://packaging.python.org/specifications/core-metadata/#name
+    name='dcos-jupyter-tensorboard-extension',  # Required
 
-class NPM(Command):
-    description = 'install package.json dependencies using npm'
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    #
+    # For a discussion on single-sourcing the version across setup.py and the
+    # project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.1',  # Required
 
-    user_options = []
+    # This is a one-line description or tagline of what your project does. This
+    # corresponds to the "Summary" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#summary
+    description='A Jupyter & JupyterLab extension which acts a proxy for the TensorBoard UI on DC/OS',  # Required
 
-    node_modules = pjoin(node_root, 'node_modules')
+    # This is an optional longer description of your project that represents
+    # the body of text which users will see when they visit PyPI.
+    #
+    # Often, this is the same as your README, so you can just read it in from
+    # that file directly (as we have already done above)
+    #
 
-    def initialize_options(self):
-        pass
+    # This should be a valid link to your project's main homepage.
+    #
+    # This field corresponds to the "Home-Page" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
+    url='https://github.com/fabianbaier/dcos-jupyter-tensorboard-extension',  # Optional
 
-    def finalize_options(self):
-        pass
+    # This should be your name or the name of the organization which owns the
+    # project.
+    author='Fabian Baier',  # Optional
 
-    def has_npm(self):
-        try:
-            check_call(['npm', '--version'])
-            return True
-        except Exception:
-            return False
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email='fabian@mesosphere.com',  # Optional
 
-    def should_run_npm_install(self):
-        node_modules_exists = os.path.exists(self.node_modules)
-        return self.has_npm() and not node_modules_exists
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
-    def should_run_npm_pack(self):
-        return self.has_npm()
-
-    def run(self):
-        has_npm = self.has_npm()
-        if not has_npm:
-            log.error("`npm` unavailable.  If you're running this command using sudo, make sure `npm` is available to sudo")
-
-        env = os.environ.copy()
-        env['PATH'] = npm_path
-
-        if self.should_run_npm_install():
-            log.info("Installing build dependencies with npm.  This may take a while...")
-            check_call(['npm', 'install'], cwd=node_root, stdout=sys.stdout, stderr=sys.stderr)
-            os.utime(self.node_modules, None)
-
-        if self.should_run_npm_pack():
-            check_call(['npm', 'pack', node_root], cwd=pjoin(here, 'ipympl'), stdout=sys.stdout, stderr=sys.stderr)
-
-        files = glob.glob(tar_path)
-        self.targets.append(tar_path if not files else files[0])
-
-        for t in self.targets:
-            if not os.path.exists(t):
-                msg = 'Missing file: %s' % t
-                if not has_npm:
-                    msg += '\nnpm is required to build a development version of widgetsnbextension'
-                raise ValueError(msg)
-
-        self.distribution.data_files = get_data_files()
-
-        # update package data in case this created new files
-        update_package_data(self.distribution)
-
-setup_dict = dict(
-    name='dcos_jupyter_tensorboard_extension',
-    description='A Jupyter & JupyterLab extension which acts a proxy for the TensorBoard UI',
-    packages=find_packages(),
-    author          = 'Fabian Baier',
-    author_email    = 'fabian@mesophere.com',
-    url             = 'https://github.com/fabianbaier/dcos-jupyter-tensorboard-extension',
-    license         = 'BSD',
-    platforms       = "Linux, Mac OS X, Windows",
-    keywords        = ['Jupyter', 'JupyterLab', 'TensorBoard'],
-    python_requires = '>=3.5',
-    classifiers     = [
+        # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
-    install_requires=[
-        'notebook'
-    ]
-)
 
-try:
-    ensure_python(setup_dict["python_requires"].split(','))
-except ValueError as e:
-    raise  ValueError("{:s}, to use {} you must use python {} ".format(
-                          e,
-                          setup_dict["name"],
-                          setup_dict["python_requires"])
-                     )
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='jupyter-server-extension jupyter-lab-extension mesosphere dcos tensorboard tensorflow',  # Optional
 
-from serverextension.dcos_jupyter_tensorboard_serverextension import __version__
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
 
-setuptools.setup(
-    version=__version__,
-    **setup_dict
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['ipython >= 6',
+                      'notebook >= 5',
+                      'widgetsnbextension'],  # Optional
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    #
+    # If using Python 2.6 or earlier, then these have to be included in
+    # MANIFEST.in as well.
+    package_data={  # Optional
+        'sample': ['package_data.dat'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    #data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/pypa/sampleproject/',
+    },
 )
